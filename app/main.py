76b09f03fa78ececb45a8e8caa8052d8e2346f64
alex <sanyabulahov@icloud.com>
2025-07-13 14:08:22 +0400

    Init main file

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from app.auth.routes import auth_router
+from app.api.routes import dashboard_router
+from app.api.employee_routes import router as employee_router
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+app.include_router(auth_router)
+app.include_router(dashboard_router)
+
+from app.core.database import engine
+from app.models.user import Base as UserBase
+
+UserBase.metadata.create_all(bind=engine)
+app.include_router(employee_router)
